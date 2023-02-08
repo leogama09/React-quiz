31fdeadc684f5b0eef9bcc37be188b77afce1799
Leonardo Gama <leogama09@gmail.com>
2023-02-08 12:13:21 -0300

    Create Reducer

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -1,9 +1,31 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions'
+
+const STAGES = ["Start", "Playing", "End"]
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+}
+
+const quizReducer =(state, action) => {
+    console.log(state, action)
+
+    switch(action.type) {
+        case "CHANGE_STATE":
+            console.log("caiu")
+            return state
+
+        default:
+            return state
+    }
+
+}
 
 export const QuizContext = createContext()
 
 export const QuizProvider = ({children}) => {
-    const value = {name: "Quiz"}
+    const value = useReducer(quizReducer, initialState)
 
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
