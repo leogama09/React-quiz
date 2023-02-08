6fcbfa625dcbc55c597b6d4e231ee2033b734146
Leonardo Gama <leogama09@gmail.com>
2023-02-08 14:32:00 -0300

    Create GameOver.jsx

---
+++ quiz/src/components/GameOver.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import { QuizContext } from "../context/quiz"
+
+import WellDone from '../img/welldone.svg'
+
+import './GameOver.css'
+
+const GameOver = () => {
+  return (
+    <div id="gameover">
+        <h2>Fim de Jogo!</h2>
+        <p>Pontuação: x</p>
+        <p>Você acertou y de z perguntas.</p>
+        <img src={WellDone} alt="Fim do Quiz" />
+        <button>Reiniciar</button>
+    </div>
+  )
+}
+
+export default GameOver
