e741df6fb3566166a1933055d6db54bc57d3927c
Leonardo Gama <leogama09@gmail.com>
2023-02-08 14:00:53 -0300

    Update Question.jsx

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -4,11 +4,17 @@ import { QuizContext } from "../context/quiz"
 
 const Questions = () => {
   const [quizState, dispatch] = useContext(QuizContext)
-
-  console.log(quizState)
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
 
   return (
-    <div>Question</div>
+    <div id="question">
+      <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
+    </div>
   )
 }
 
