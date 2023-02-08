e6c8e2390d413d89b397f36e6632842fe3725cf0
Leonardo Gama <leogama09@gmail.com>
2023-02-08 11:31:03 -0300

    App Structure

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,10 +1,14 @@
+// react, componentes, estáticos
+
+import Welcome from './components/Welcome'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
   )
 }
