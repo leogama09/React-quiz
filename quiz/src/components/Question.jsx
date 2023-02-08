7a36b908e2d16e13353f4a7092e36b81692d47c9
Leonardo Gama <leogama09@gmail.com>
2023-02-08 16:09:42 -0300

    Update Question.jsx

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -9,18 +9,32 @@ const Questions = () => {
   const [quizState, dispatch] = useContext(QuizContext)
   const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+  const onSelectOption = (option) => {
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: {answer: currentQuestion.answer, option}
+    })
+  }
+
   return (
     <div id="question">
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         {currentQuestion.options.map((option) => (
-          <Option option={option} key={option} />
+          <Option 
+            option={option} 
+            key={option} 
+            answer={currentQuestion.answer}
+            selectOption={() => onSelectOption(option)}
+          />
         ))}
       </div>
-      <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
+      {quizState.answerSelected && (
+        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
         Continuar
       </button>
+      )}
     </div>
   )
 }
