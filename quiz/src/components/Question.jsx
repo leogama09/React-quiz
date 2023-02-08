7a1a4fc270740b494ac5200599823e480526f4be
Leonardo Gama <leogama09@gmail.com>
2023-02-08 15:45:59 -0300

    Update Question.jsx

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react"
 import { QuizContext } from "../context/quiz"
 
+import Option from "./Option"
+
 import "./Question.css"
 
 const Questions = () => {
@@ -12,7 +14,9 @@ const Questions = () => {
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+          <Option option={option} key={option} />
+        ))}
       </div>
       <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
         Continuar
